363285323c7b6e7b8f9e6727cc1c80d02ee68c96
Sagi <sagishabon2@gmail.com>
2023-04-02 11:22:15 +0300

    update 11 - להמשיך את הזמנת ההשכרה

--- app/src/main/java/com/example/sharingchargingstations/ScheduleActivity.java
+++ app/src/main/java/com/example/sharingchargingstations/ScheduleActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.ListView;
@@ -24,12 +25,13 @@ import java.util.function.Predicate;
 
 public class ScheduleActivity extends AppCompatActivity {
     private CalendarView simpleCalendarView;
-    private ArrayList<String> hours = new ArrayList<>();
+    private ArrayList<OrderHour> hours = new ArrayList<>();
     private Model model = Model.getInstance();
     private ListView lvHours;
     private ArrayAdapter hoursArrayAdapter;
     private ChargingStation chargingStation;
     private int pos;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,8 +42,6 @@ public class ScheduleActivity extends AppCompatActivity {
 
         simpleCalendarView = findViewById(R.id.cvDate);
         long selectedDate = simpleCalendarView.getDate();
-        //simpleCalendarView.setDate(new Date().getTime());
-        //simpleCalendarView.setFirstDayOfWeek(1);
 
 
         lvHours = findViewById(R.id.lvHours);
@@ -54,25 +54,45 @@ public class ScheduleActivity extends AppCompatActivity {
 
             }
         });
-        hoursArrayAdapter = new ArrayAdapter<String>(this, R.layout.item_hour, hours) {
+        hoursArrayAdapter = new ArrayAdapter<OrderHour>(this, R.layout.item_hour, hours) {
             @NonNull
             @Override
             public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
                 View view = getLayoutInflater().inflate(R.layout.item_hour, null);
                 TextView tvHour = view.findViewById(R.id.tvHour);
-                tvHour.setText(hours.get(position));
-                if (hours.get(position).contains("Occupied")) tvHour.setBackgroundColor(Color.rgb(240,128,128));
+                tvHour.setText(hours.get(position).toString());
+                if (hours.get(position).hourStatus == HourStatus.occupied) tvHour.setBackgroundColor(Color.rgb(240,128,128));
+                if (hours.get(position).hourStatus == HourStatus.selected) tvHour.setBackgroundColor(Color.rgb(135,206,235));
+
                 return view;
             }
         };
+
         lvHours.setAdapter(hoursArrayAdapter);
+        lvHours.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                for (int i = 0; i < hours.size() ; i++) {
+                    OrderHour orderHour = hours.get(position);
+                    if (orderHour.hourStatus != HourStatus.occupied)
+                        orderHour.hourStatus = (i == position) ? HourStatus.selected : HourStatus.free;
+                }
+
+                hoursArrayAdapter.notifyDataSetChanged();
+            }
+        });
         Date date = new Date();
         date.setTime(date.getTime());
         setHoursList(date.getYear() + 1900, date.getMonth() + 1, date.getDate());
 
+
+
     }
 
     private void setHoursList(final int year, final int month, final int day) {
+        final int dateYear = year - 1900; //Java date year is since 1900 (2023 is 123)
+        final int dateMonth = year - 1900; //Java date year is since 1900 (2023 is 123)
+
         hours.clear();
         Rental rental;
         for (int i = (int) chargingStation.getMinHour(); i < (int) chargingStation.getMaxHour(); i++) {
@@ -82,7 +102,7 @@ public class ScheduleActivity extends AppCompatActivity {
                         @Override
                         public boolean test(Rental r) {
 
-                            return r.getStartDate().getYear() == year &&
+                            return r.getStartDate().getYear() == dateYear &&
                                     r.getStartDate().getMonth() == month &&
                                     r.getStartDate().getDate() == day &&
                                     r.getStartDate().getHours() == hour &&
@@ -93,10 +113,50 @@ public class ScheduleActivity extends AppCompatActivity {
                     .orElse(null);
             String tmp = rental == null ? "" : "                   Occupied";
             Toast.makeText(getApplicationContext(),"year: " + year + ", month: " + month + ", day: " + day,Toast.LENGTH_SHORT).show();
-
-            hours.add(i + ":00 - " + (i+1) + ":00" + tmp);
+            String s = i + ":00 - " + (i+1) + ":00" + tmp;
+            hours.add(new OrderHour(s, HourStatus.free));
         }
         hoursArrayAdapter.notifyDataSetChanged();
+    }
+    public void rentClick(View view){
+        //Rental rental = new Rental(chargingStation.get)
+    }
+    private enum HourStatus{
+        free,
+        occupied,
+        selected
+    }
+    private class OrderHour{
+        private String content;
+        private HourStatus hourStatus;
+
+        public OrderHour(String content, HourStatus hourStatus) {
+            this.content = content;
+            this.hourStatus = hourStatus;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public HourStatus getHourStatus() {
+            return hourStatus;
+        }
+
+        public void setHourStatus(HourStatus hourStatus) {
+            this.hourStatus = hourStatus;
+        }
+
+        @Override
+        public String toString() {
+            String s = hourStatus == HourStatus.selected ? " Selected" : "";
+            return content + s;
+
+        }
     }
 
 }
